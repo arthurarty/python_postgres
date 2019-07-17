7519a10ccfcae912a872b103ed3866937281cfb9
arthurarty <arthur.nangai@gmail.com>
2019-07-17 15:48:17 +0300

    inner join with search

--- inner_join.py
+++ inner_join.py
@@ -8,3 +8,10 @@ cur.execute('''
         INNER JOIN DEPARTMENT
         ON COMPANY.ID = DEPARTMENT.EMP_ID;''')
 print(cur.fetchall())
+
+cur.execute('''
+        SELECT
+        ADDRESS FROM COMPANY
+        INNER JOIN DEPARTMENT
+        ON COMPANY.ID = DEPARTMENT.EMP_ID WHERE DEPT='Engineering';''')
+print(cur.fetchall())
